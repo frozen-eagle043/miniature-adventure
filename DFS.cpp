d002dc9d4e58737ea52ecc5623fb49ff54727051
Frozen_Eagle <55057194+frozen-eagle043@users.noreply.github.com>
2021-05-10 10:49:09 +0530

    Update DFS.cpp

--- DFS.cpp
+++ DFS.cpp
@@ -1,46 +1,50 @@
 #include <iostream>
-#include <unordered_set>
-#include <queue>
 #include <utility>
 using namespace std;
-unordered_set < int >visitedNODES;
-void BFS_ALGO (int n, int adjacency_matrix[5][5], int STARTINGnode)
+
+bool visitedNODES[5] = { false };
+
+void DFS_ALGO (int i, int adjacency_matrix[5][5], int n)
 {
-  queue < int >q;
-  q.push (STARTINGnode);
-  cout << STARTINGnode << "->";
-  visitedNODES.insert (STARTINGnode);
-  while (q.size () != 0)
+  for (int j = 0; j < n; j++)
     {
-      int i = q.front ();
-      q.pop ();
-      for (int start = 0; start < 5; start++)
+      if (adjacency_matrix[i][j] == 1)
 	{
-	  if (adjacency_matrix[i][start] == 1)
-	    {
-	      if (visitedNODES.find (start) != visitedNODES.end ())
-		continue;
-	      q.push (start);
-	      cout << start << "->";
-	      visitedNODES.insert (start);
-	    }
+	  if (visitedNODES[j] == true)
+	    continue;
+	  visitedNODES[j] = true;
+      char t = 'A' + j;
+	  cout << t << "->";
+	  DFS_ALGO (j, adjacency_matrix, 5);
 	}
     }
 }
 
+bool isConnected (int n, int adjacency_matrix[5][5])
+{
+  visitedNODES[0] = true;
+  cout << "A" << "->";
+  DFS_ALGO (0, adjacency_matrix, n);
+  for (int i = 0; i < 5; i++)
+    {
+      if (visitedNODES[i] == false)
+	return false;
+    }
+  return true;
+}
 int main ()
 {
   int adjacency_matrix[5][5] = 
   {
-    {0, 1, 1, 0, 0},
-    {0, 0, 1, 1, 1},
-    {0, 0, 0, 1, 0},
-    {0, 0, 0, 0, 0},
-    {0, 0, 0, 1, 0}
+    {0, 1, 1, 0, 1},
+    {1, 0, 0, 1, 1},
+    {1, 0, 0, 0, 0},
+    {0, 1, 0, 0, 0},
+    {1, 1, 0, 0, 0}
   };
-  int STARTINGnode;
-  cout<<"ENTER the STARTING NODE : ";
-  cin >> STARTINGnode;
-  BFS_ALGO (5, adjacency_matrix, STARTINGnode);
+  if(isConnected (5, adjacency_matrix) )
+  cout<<"\nConnected\n" ;
+  else
+  cout<<"\nDisconnected\n";
   return 0;
 }
